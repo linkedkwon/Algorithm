5d2570d7af427ab3ec6efe28fc6229de2a561a61
linkedkwon <linkedkwon@gmail.com>
2021-10-27 19:50:04 +0900

    2×n 타일링

---
+++ baekjoon/dynamic_programming/P11726.java
@@ -0,0 +1,25 @@
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11726 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+2];
+        dp[1] = 1; dp[2] = 2;
+
+        for(int i = 1; i < n + 1; i++){
+            if(i == 1 || i == 2)
+                continue;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+        br.close();
+    }
+}
