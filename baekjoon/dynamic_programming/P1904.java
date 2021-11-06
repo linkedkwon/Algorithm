5cea82eafa6557cd94263de8d4b16e84afa8e1f5
linkedkwon <linkedkwon@gmail.com>
2021-11-06 11:36:08 +0900

    01타일

---
+++ baekjoon/dynamic_programming/P1904.java
@@ -0,0 +1,21 @@
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1904 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 2];
+        dp[1] = 1; dp[2] = 2;
+
+        for(int i = 3; i < N + 1; i++)
+            dp[i] = (dp[i - 1] +  dp[i - 2]) % 15746;
+
+        System.out.println(dp[N]);
+        br.close();
+    }
+}
