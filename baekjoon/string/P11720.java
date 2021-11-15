4d91b2c110709c521cbfe19ab64329d838b27990
linkedkwon <linkedkwon@gmail.com>
2021-11-16 08:11:28 +0900

    숫자의 합

---
+++ baekjoon/string/P11720.java
@@ -0,0 +1,21 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11720 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int sum = 0;
+        for(int i = 0; i < N; i++)
+            sum += str.charAt(i) - '0';
+
+        System.out.println(sum);
+        br.close();
+    }
+}
