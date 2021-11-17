3c0bf776945b6dc93feecd745a36f8e5ecdcd6c9
linkedkwon <linkedkwon@gmail.com>
2021-11-18 05:47:47 +0900

    문자열 반복

---
+++ baekjoon/string/P2675.java
@@ -0,0 +1,27 @@
+package baekjoon.string;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P2675 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+
+            for(int j = 0; j < str.length(); j++){
+                for(int k = 0; k < R; k++)
+                    bw.write(str.charAt(j));
+            }
+            bw.write("\n");
+        }
+        br.close();
+        bw.close();
+    }
+}
