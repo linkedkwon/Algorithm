a1af20505c67df5108c08776fb258a436d709367
linkedkwon <linkedkwon@gmail.com>
2021-12-08 22:36:07 +0900

    저울

---
+++ baekjoon/greedy/P2437.java
@@ -0,0 +1,33 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P2437 {
+
+    public static  void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(sum + 1 < arr[i])
+                break;
+            sum += arr[i];
+        }
+
+        System.out.println(sum + 1);
+        br.close();
+    }
+}
