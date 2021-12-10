751f9b7e294a4b290b90b536b243b2a0bf022560
linkedkwon <linkedkwon@gmail.com>
2021-12-11 08:15:15 +0900

    요세푸스 문제

---
+++ baekjoon/queue/P1158.java
@@ -0,0 +1,36 @@
+package baekjoon.queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+//circular queue(using list, size mod)
+public class P1158 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken()) - 1;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 1; i < N + 1; i++)
+            arr.add(i);
+
+        StringBuilder sb = new StringBuilder();
+        int dx = K;
+        while(true){
+            if(dx >= arr.size())
+                dx %= arr.size();
+            sb.append(arr.remove(dx));
+            if(arr.size() == 0)
+                break;
+            sb.append(", ");
+            dx += K;
+        }
+
+        System.out.println("<" + sb + ">");
+        br.close();
+    }
+}
