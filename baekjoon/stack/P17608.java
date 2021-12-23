e3ce8de735f028dbae919db031200ab2e01da30e
linkedkwon <linkedkwon@gmail.com>
2021-12-23 22:44:47 +0900

    막대기

---
+++ baekjoon/stack/P17608.java
@@ -0,0 +1,36 @@
+package baekjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class P17608 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(Integer.parseInt(br.readLine()));
+        for(int i = 1; i < N; i++){
+            int h = Integer.parseInt(br.readLine());
+
+            while(!stack.isEmpty()){
+                if(stack.peek() <= h) {
+                    stack.pop();
+                }
+                else{
+                    stack.push(h);
+                    break;
+                }
+            }
+
+            if(stack.isEmpty())
+                stack.push(h);
+        }
+
+        System.out.println(stack.size());
+        br.close();
+    }
+}
