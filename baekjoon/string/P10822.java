d1fa5bf5230d5414d008d4f59ab4b4fab6dfa45d
linkedkwon <linkedkwon@gmail.com>
2022-01-11 09:20:07 +0900

    더하기

---
+++ baekjoon/string/P10822.java
@@ -0,0 +1,21 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10822 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(",");
+
+        int sum = 0;
+        for(int i = 0; i < str.length; i++)
+            sum += Integer.parseInt(str[i]);
+
+        System.out.println(sum);
+        br.close();
+    }
+
+}
