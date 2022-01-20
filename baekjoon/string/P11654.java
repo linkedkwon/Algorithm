ba081606edc8bf1a643f6d3a504cd8be4e61508b
linkedkwon <linkedkwon@gmail.com>
2022-01-20 17:01:44 +0900

    아스키 코드

---
+++ baekjoon/string/P11654.java
@@ -0,0 +1,15 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11654 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().charAt(0) - 0);
+        br.close();
+    }
+
+}
