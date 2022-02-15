101b1a7a89f84c8bb0acefad2b13337b3a058c91
linkedkwon <linkedkwon@gmail.com>
2022-02-15 15:09:21 +0900

    먹을 것인가 먹힐 것인가

---
+++ baekjoon/two_pointer/P16472.java
@@ -0,0 +1,56 @@
+package baekjoon.two_pointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P16472 {
+
+    static int N, M, CNT;
+    static int[] A, B;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++) {
+            readIn();
+            Arrays.sort(A);
+            Arrays.sort(B);
+            tp();
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+    static void readIn() throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
+        A = new int[N]; B = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++)
+            A[i] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++)
+            B[i] = Integer.parseInt(st.nextToken());
+
+        CNT = 0;
+    }
+
+    static void tp(){
+        int dx = 0, dy = 0;
+        while(dx < N && dy < M){
+            if(A[dx] <= B[dy]){
+                dx++;
+            }else{
+                CNT += N - dx;
+                dy++;
+            }
+        }
+        sb.append(CNT).append('\n');
+    }
+
+}
