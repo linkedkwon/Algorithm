955296ecc035c565b4c946c3a35eb85227f1d420
linkedkwon <linkedkwon@gmail.com>
2022-02-23 15:15:48 +0900

    차이를 최대로

---
+++ baekjoon/backtracking/P10819.java
@@ -0,0 +1,52 @@
+package baekjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P10819 {
+
+    static int N, MAX;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException{
+        readIn();
+        dfs(0, new int[N]);
+        System.out.println(MAX);
+    }
+
+    static void readIn() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N]; visited = new boolean[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        br.close();
+    }
+
+    static void dfs(int depth, int[] tmp){
+        if(depth == N){
+            int sum = 0;
+            for(int i = 0; i < N - 1; i++)
+                sum += Math.abs(tmp[i] - tmp[i + 1]);
+
+            MAX = Math.max(MAX, sum);
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                tmp[depth] = arr[i];
+                dfs(depth + 1, tmp);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
