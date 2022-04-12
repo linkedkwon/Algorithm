ac2fb4f07aff2eadb7335620ec84ba32c03e2038
linkedkwon <linkedkwon@gmail.com>
2022-04-12 16:24:00 +0900

    고양이

---
+++ baekjoon/implementation/P10171.java
@@ -0,0 +1,11 @@
+package baekjoon.implementation;
+
+public class P10171 {
+
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append('\n').append(" )  ( ')").append('\n').append("(  /  )").append('\n').append(" \\(__)|");
+        System.out.println(sb);
+    }
+
+}
